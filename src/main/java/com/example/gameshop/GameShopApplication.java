c3e25f67df43fac53d67e2e6a55e6bb4a19c66d4
Diploj <pavel.barkov.04@bk.ru>
2026-04-07 13:29:29 +0300

    start task1

--- src/main/java/com/example/gameshop/GameShopApplication.java
+++ src/main/java/com/example/gameshop/GameShopApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GameShopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameShopApplication.class, args);
+    }
 
 }
